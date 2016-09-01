ee8e5771b39f5286896b188d3cb2812df4d9b712
Nguyen Manh Tung <nguyenmanhtung848@gmail.com>
2016-09-01 11:16:54 +0700

    5 minutes quick start app from angular tutorial.

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+/*when we need some thing, we import it*/
+import { Component } from '@angular/core';
+
+//meta data tells angular how to create this Component
+@Component({
+    selector: 'my-app',
+    template: '<h1>My First Angular 2 App!!</h1>'
+})
+
+export class AppComponent { }
